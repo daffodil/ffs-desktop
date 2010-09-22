1fdd26ac9a047eaf40447ca9c2c1a8d0b546d5ca
Peter Mash Morgan <ac001@daffodil.uk.com>
2010-09-22 13:34:05 +0100

    oops

--- ffs-desktop/flightgear/aircraftwidget.cpp
+++ ffs-desktop/flightgear/aircraftwidget.cpp
@@ -1,9 +1,27 @@
+
+/**
+  This widget displays a list of aircraft,
+    does a background call to a process that executes FG
+  */
+
 #include "aircraftwidget.h"
 
+// layouts
 #include <QtGui/QVBoxLayout>
 #include <QtGui/QHBoxLayout>
+
 #include <QtGui/QLabel>
 
+#include <QtGui/QToolBar>
+
+//* tree
+#include <QtGui/QHeaderView>
+#include <QtGui/QTreeWidgetItem>
+#include <QtGui/QAction>
+/*
+
+
+*/
 
 AircraftWidget::AircraftWidget(QWidget *parent) :
     QWidget(parent)
@@ -12,19 +30,35 @@ AircraftWidget::AircraftWidget(QWidget *parent) :
     QVBoxLayout *mainLayout = new QVBoxLayout();
     setLayout(mainLayout);
 
+    //** Top Toolbar
+    QToolBar *topToolBar = new QToolBar();
+    mainLayout->addWidget(topToolBar);
 
 
-    QLabel *headerLabel = new QLabel("Aircraft");
+    QLabel *headerLabel = new QLabel(tr("Aircraft"));
     mainLayout->addWidget(headerLabel);
     
     QHBoxLayout *hBox = new QHBoxLayout();
     mainLayout->addLayout(hBox);
     
-    //* Aircraft Tree
+    //**********************************************************************
+    //* Aircraft Tree View
+    // TOSO make nested on xml files, for now its a straight list
     treeWidget = new QTreeWidget();
     hBox->addWidget(treeWidget);
 
+    //** get handle on TreeHeader item and set the labels
+    QTreeWidgetItem *headerItem = treeWidget->headerItem();
+    headerItem->setText(0, tr("Shell"));
+    headerItem->setText(1, tr("Description"));
+
+    //* Get a handle on the header and stretch the last section
     QHeaderView *treeHeader = treeWidget->header();
     treeHeader->setStretchLastSection(true);
+
+
+
+
+
 
 }
