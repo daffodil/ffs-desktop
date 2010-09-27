503f2b2321ea965a0ec65ef7631f8a09c2ca204a
Peter Mash Morgan <ac001@daffodil.uk.com>
2010-09-27 15:15:16 +0100

    Need adb advice

--- ffs-desktop/airports/aptdatparser.cpp
+++ ffs-desktop/airports/aptdatparser.cpp
@@ -1,12 +1,12 @@
 
 /*
-  Peels of the robin layer and gets out teh airports and runways
+  Peel of the robin and gets out the airports and runways and minimal hopefully
 
   TODO incomplete list
 
   step 1
 
-  lonly load the ICAO aiports, ignore heliports and seway etc.. lite wiegt and wise..
+  only load the ICAO aiports, ignore heliports and seway etc.. lite wiegt and wise..
 
   assumed is ils etc..
   Other aiports whousl be available on demand as a parse etc..
@@ -83,30 +83,6 @@ AptDatParser::AptDatParser(QObject *parent) :
             queryT.exec("CREATE TABLE airports(airport varchar(10) NOT NULL PRIMARY KEY, name varchar(50) NULL, elevation int, tower tinyint NULL, INDEX (name)) ");
             queryT.exec("CREATE TABLE runways(airport varchar(10) NULL, runway varchar(3), INDEX (airport, runway))");
       }
-      /*
-    while (queryT.next()) {
-             QString name = queryT.value(0).toString();
-             //int salary = query.value(1).toInt();
-             qDebug() << name ;//<< salary;
-     }*/
-    //return;
-   // qDebug() << "db.open= " << dbOk;
-//    QSqlQuery query;
-//    bool ok = query.exec("SELECT * FROM airports");
-//    if(ok){
-//        while (query.next()) {
-//                 QString name = query.value(0).toString();
-//                // int salary = query.value(1).toInt();
-//                 qDebug() << name ;
-//         }
-//    }else{
-//       qDebug() << query.lastError();
-//    }
-    //while (query.next())
-   // {
-    //  qDebug("NO");
-    //}
-
 }
 void AptDatParser::import_aptdat(){
 
@@ -229,53 +205,3 @@ void AptDatParser::import_aptdat(){
         //process_line(line);
     }
 }
-
-//    self.read_line()
-//            self.read_line()
-//            self.read_line()
-//
-//            airports = {}
-//            c = 0
-//            while 1:
-//                    line = self.read_line()
-//                    #print c, line
-//                    #print line
-//                    if line != '':
-//                            cols = line.split()
-//                            ## aiport, seaplane, heliport
-//                            akey = cols[0]
-//
-//                            heliports = []
-//                            seaports = []
-//                            if akey != '99':
-//                                    if akey in ['1', '16', '17']:
-//                                            seaport = True if akey == '16' else False
-//                                            heliport = True if akey == '17' else False
-//                                            #                            icoa,    name,    seaport, heliport, elev,    atc
-//                                            #print cols
-//                                            airport = " ".join(cols[5:])
-//                                            icao = str(cols[4])
-//                                            #self.airports_csv.writerow( [cols[4], airport, seaport, heliport, cols[1], cols[2]] )
-//                                            airports[icao] = {
-//                                                            'name': str(airport),
-//                                                            'seaport': seaport,
-//                                                            'heliport': heliport,
-//                                                            'elevation': int(cols[1]),
-//                                                            'atc': bool(cols[2])
-//                                            }
-//
-//                                            #yaml.dump(yam, self.airports_yaml, width=500, indent=4)
-//                                            if c % 100 == 0:
-//                                                    print c, cols[4], airport
-//                                            if c == 1000:
-//                                                    print "break"
-//                                                    break
-//                                            c += 1
-//            sorted_airports = sorted(airports.keys())
-//            print sorted_airports
-//            for ki in sorted_airports:
-//                    data = {ki: airports[ki]}
-//                    yaml.dump(data, self.airports_yaml, width=500, indent=4)
-//            print "### done ####"
-//            self.close()
-
